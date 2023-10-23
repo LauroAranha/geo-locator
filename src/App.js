90d70a1d2ce5f916f21cb235d81fbb9438c1d61c
lauroaranha <lauroaranha.0@gmail.com>
2023-10-22 22:22:37 -0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [location, setLocation] = useState([]);
+
+  useEffect(() => {
+    function success(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation([position.coords.latitude, position.coords.longitude]);
+    }
+
+    function error() {
+      alert("Sorry, no position available.");
+    }
+
+    const options = {
+      enableHighAccuracy: true,
+      maximumAge: 30000,
+      timeout: 27000,
+    };
+
+    function updateLocation() {
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+    updateLocation();
+
+    const interval = setInterval(updateLocation, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>{location[0]}</p>
+        <p>{location[1]}</p>
       </header>
     </div>
   );
